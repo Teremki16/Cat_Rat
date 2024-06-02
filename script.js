2387b7a900dd07f351e22599802d5da796cac7fc
Teremki16 <teremki16robocode@gmail.com>
2024-06-02 15:51:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+$(".")
+
+
+
+
+
+
+new Date().getTime()
+
+
+new Date(data).toDateString()
+
+
+localStorage.setItem("notes", JSON.stringify(data))
+
+
+JSON.parse(localStorage.getItem("notes"))
+
+
+
+
+
+
+
+
+
+
+
+
+});
